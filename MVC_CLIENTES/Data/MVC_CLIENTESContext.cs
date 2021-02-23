4d45d1450db7a79f2caae966973c03ff371d544b
Sofía <sofiacm17@hotmail.com>
2021-02-23 19:37:29 +0100

    Agregar archivos de proyecto.

---
+++ MVC_CLIENTES/Data/MVC_CLIENTESContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_CLIENTES.Models;
+
+namespace MVC_CLIENTES.Data
+{
+    public class MVC_CLIENTESContext : DbContext
+    {
+        public MVC_CLIENTESContext (DbContextOptions<MVC_CLIENTESContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_CLIENTES.Models.Cliente> Cliente { get; set; }
+    }
+}
---
+++ MVC_CLIENTES/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CLIENTES.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+
+        [Display(Name = "Dirección")]
+        public string Direccion { get; set; }
+       
+        [Display(Name = "Teléfono")]
+        public string Telefono { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name ="Fecha de alta")]
+        public DateTime FechaAlta { get; set; }
+    }
+}
